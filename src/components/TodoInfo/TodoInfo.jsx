60e04c5786cfabe4f29c155f27d516f4eebb7045
Dmytro Shovheniuk <dimashovgenyuk06@gmail.com>
2026-05-10 18:30:26 -0500

    test 2

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,3 +1,12 @@
-// Add the required props
+import { UserInfo } from '../UserInfo/UserInfo';
 
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo }) => {
+  return (
+    <article
+      className={`TodoInfo${todo.completed ? ' TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,8 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({ user }) => {
+  return (
+    <>
+      <p className="UserInfo__email">{user.email}</p>
+      <p className="UserInfo__name">{user.name}</p>
+    </>
+  );
+};
